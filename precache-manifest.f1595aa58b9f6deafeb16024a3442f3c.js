a672958685f3164b9606f699dac5c2234953ffc2
abhinavkorpal <abhinavkorpaljv@gmail.com>
2020-05-19 15:34:37 +0530

    Updates

---
+++ precache-manifest.f1595aa58b9f6deafeb16024a3442f3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6d416622f68c1f8947a8e896aa2637",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5852edec3afaa9e987e0",
+    "url": "/home/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/home/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5852edec3afaa9e987e0",
+    "url": "/home/static/js/main.d978b9a3.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "206d2929dc8ac00572d47daea44551da",
+    "url": "/home/static/media/resume.206d2929.pdf"
+  }
+]);
